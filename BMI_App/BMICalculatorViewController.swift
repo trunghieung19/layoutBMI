9c60c1c536e00b30a0687ee8326d01e22ab11104
trunghieung19 <66891129+trunghieung19@users.noreply.github.com>
2020-11-08 09:59:31 +0700

    layoutBMI

---
+++ BMI_App/BMICalculatorViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BMICalculatorViewController.swift
+//  BMI_App
+//
+//  Created by Apple on 11/8/20.
+//  Copyright © 2020 trunghieu. All rights reserved.
+//
+
+import UIKit
+
+class BMICalculatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ BMI_App/ResultBMIViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ResultBMIViewController.swift
+//  BMI_App
+//
+//  Created by Apple on 11/8/20.
+//  Copyright © 2020 trunghieu. All rights reserved.
+//
+
+import UIKit
+
+class ResultBMIViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
